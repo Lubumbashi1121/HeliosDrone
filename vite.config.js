97f351e145eda7c99cbfc65dc2f8a9f5d310da4a
Lubumbashi1121 <nagyakos97@gmail.com>
2025-01-28 17:07:07 +0100

    Resolve merge conflicts in vite.config.js and index.html

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/drone-website",
 })
-=======
-  base: '/drone-website',
-})
->>>>>>> 65ffdecd4c31500ef550e28facf05420a52060b7
