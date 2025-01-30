1bb0ba592e9d768d5e670f7fa40f0cdabbe12b75
Lubumbashi1121 <nagyakos97@gmail.com>
2025-01-30 16:07:56 +0100

    Updated configuration for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Use this for React 18
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,7 +9,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Modern React 18 rendering
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter basename="/drone-website">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
