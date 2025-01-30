91acc0ffe55b7d4b111c35b082e7469b6fd142f5
Lubumbashi1121 <nagyakos97@gmail.com>
2025-01-30 17:31:49 +0100

    Stop tracking dist folder and update .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Use this for React 18
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +8,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Modern React 18 rendering
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/drone-website/">
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
 );
